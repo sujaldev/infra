79fa8d7ac422b3f20c8e99fe1b18e8236f3e04d0
Sujal Singh <email.sujalsingh@gmail.com>
2026-09-04 11:34:34 +0530

    make each step in init.sh a function

--- erpnext/init.sh
+++ erpnext/init.sh
@@ -16,52 +16,55 @@ export DB_PASSWORD="${DB_PASSWORD:-$(openssl rand -hex 32)}"
 export GUNICORN_WORKERS="${GUNICORN_WORKERS:-$(( $(nproc) * 2 + 1 ))}"
 export NGINX_PROXY_HOSTS="${NGINX_PROXY_HOSTS:?NGINX_PROXY_HOSTS environment variable cannot be empty}"
 
-############################################################
+create_user() {
+  if id "$SERVICE_USERNAME" &>/dev/null; then
+    echo "User '$SERVICE_USERNAME' already exists!"
+    exit 1
+  else
+    useradd \
+      --user-group \
+      --shell /usr/sbin/nologin \
+      --home-dir "$SERVICE_HOME" \
+      --create-home \
+      "$SERVICE_USERNAME"
 
-# Create user
-if id "$SERVICE_USERNAME" &>/dev/null; then
-  echo "User '$SERVICE_USERNAME' already exists!"
-  exit 1
-else
-  useradd \
-    --user-group \
-    --shell /usr/sbin/nologin \
-    --home-dir "$SERVICE_HOME" \
-    --create-home \
-    "$SERVICE_USERNAME"
+    loginctl enable-linger "$SERVICE_USERNAME"
+  fi
+}
 
-  loginctl enable-linger "$SERVICE_USERNAME"
-fi
+generate_env() {
+  env_path="$SERVICE_HOME/.env"
+  create_env_file=1
 
-############################################################
+  if [[ -e "$env_path" ]]; then
+      read -r -p "'$env_path' already exists. Overwrite? (y/N): " answer
+      if [[ "$answer" != [yY] ]]; then
+        create_env_file=0
+      fi
+  fi
 
-# Generate .env
-env_path="$SERVICE_HOME/.env"
-create_env_file=1
+  if (( create_env_file )); then
+    # shellcheck disable=SC2016
+    envsubst '$DB_PASSWORD $GUNICORN_WORKERS $FRAPPE_SITE_NAME_HEADER $NGINX_PROXY_HOSTS' < template.env > "$env_path"
+    chmod 600 "$env_path"
+  fi
+}
 
-if [[ -e "$env_path" ]]; then
-    read -r -p "'$env_path' already exists. Overwrite? (y/N): " answer
-    if [[ "$answer" != [yY] ]]; then
-      create_env_file=0
-    fi
-fi
+copy_files() {
+  cp -r ./frappe_docker "$SERVICE_HOME"
+  cp apps.json "$SERVICE_HOME"
+  correct_owner
+}
 
-if (( create_env_file )); then
-  # shellcheck disable=SC2016
-  envsubst '$DB_PASSWORD $GUNICORN_WORKERS $FRAPPE_SITE_NAME_HEADER $NGINX_PROXY_HOSTS' < template.env > "$env_path"
-  chmod 600 "$env_path"
-fi
+correct_owner() {
+  chown -R "$SERVICE_USERNAME:$SERVICE_USERNAME" \
+    "$SERVICE_HOME/frappe_docker" \
+    "$SERVICE_HOME/apps.json" \
+    "$SERVICE_HOME/.env"
+}
 
-############################################################
-
-# Copy files
-cp -r ./frappe_docker "$SERVICE_HOME"
-cp apps.json "$SERVICE_HOME"
-
-############################################################
-
-# Correct owner:group
-chown -R "$SERVICE_USERNAME:$SERVICE_USERNAME" \
-  "$SERVICE_HOME/frappe_docker" \
-  "$SERVICE_HOME/apps.json" \
-  "$SERVICE_HOME/.env"
+init() {
+  create_user
+  generate_env
+  copy_files
+}
