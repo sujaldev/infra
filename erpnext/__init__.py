5137cf78ee362f6a7de1f4d029b088b779a51b4c
Sujal Singh <email.sujalsingh@gmail.com>
2026-09-05 17:37:42 +0530

    render .env template during erpnext setup

--- erpnext/__init__.py
+++ erpnext/__init__.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 
+from pyinfra import host
+from pyinfra.facts.hardware import Cpus
+from pyinfra.operations import files
 from pyinfra.operations import server
 
 from cli.utils import ServiceCommandRegistry
@@ -12,12 +15,33 @@ cli = ServiceCommandRegistry(
 DEFAULT_SERVICE_USER = "erpnext"
 DEFAULT_SERVICE_HOME = Path("/srv") / DEFAULT_SERVICE_USER
 
+SOURCE_DIR = Path(__file__).parent.resolve()
+
+
+def generate_env(service_user: str, service_home: Path, nginx_proxy_hosts: str, gunicorn_workers: int):
+    # noinspection bad-argument-type
+    files.template(
+        src=str(SOURCE_DIR / "template.env.jinja"),
+        dest=str(service_home / ".env"),
+        mode=600,
+        user=service_user,
+        group=service_user,
+        _sudo=True,
+        _sudo_user=service_user,
+        service_home=service_home,
+        gunicorn_workers=gunicorn_workers,
+        nginx_proxy_hosts=nginx_proxy_hosts,
+    )
+
 
 @cli.command(
+    gunicorn_workers="Set to 0 to automatically calculate with the formula (2 x number of CPU cores) + 1.",
     service_user=f"Username of the host user that will run the rootless container.",
     service_home=f"Path to the home directory of the service user.",
 )
 def setup(
+        nginx_proxy_hosts: str,
+        gunicorn_workers: int = 0,
         service_user: str = DEFAULT_SERVICE_USER,
         service_home: Path = DEFAULT_SERVICE_HOME
 ):
@@ -33,6 +57,11 @@ def setup(
         ensure_home=True,
         _sudo=True,
     )
+
+    if gunicorn_workers == 0:
+        gunicorn_workers = host.get_fact(Cpus) * 2 + 1
+
+    generate_env(service_user, service_home, nginx_proxy_hosts, gunicorn_workers)
 
 
 @cli.command()
