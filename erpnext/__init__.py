791b4b08bb40953be2f43f3aad15553a69902d9e
Sujal Singh <email.sujalsingh@gmail.com>
2026-09-05 17:33:36 +0530

    add service user creation in erpnext setup

--- erpnext/__init__.py
+++ erpnext/__init__.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+
+from pyinfra.operations import server
+
 from cli.utils import ServiceCommandRegistry
 
 cli = ServiceCommandRegistry(
@@ -5,13 +9,30 @@ cli = ServiceCommandRegistry(
     help_str="Manage ERPNext deployment."
 )
 
+DEFAULT_SERVICE_USER = "erpnext"
+DEFAULT_SERVICE_HOME = Path("/srv") / DEFAULT_SERVICE_USER
 
-@cli.command()
-def setup():
+
+@cli.command(
+    service_user=f"Username of the host user that will run the rootless container.",
+    service_home=f"Path to the home directory of the service user.",
+)
+def setup(
+        service_user: str = DEFAULT_SERVICE_USER,
+        service_home: Path = DEFAULT_SERVICE_HOME
+):
     """
     Performs initial setup required to deploy ERPNext on a fresh server.
     """
-    pass
+    server.user(
+        name=f"Ensure service user {service_user!r} exists",
+        user=service_user,
+        home=str(service_home),
+        shell="/usr/sbin/nologin",
+        create_home=True,
+        ensure_home=True,
+        _sudo=True,
+    )
 
 
 @cli.command()
